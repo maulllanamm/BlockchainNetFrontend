1e7ce005d4d67a903d75557db97db69939a3e015
maulllanamm <maulllanamuhammad@gmail.com>
2025-05-13 16:14:25 +0700

    fix : navbar name

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import MainLayout from "./layouts/MainLayout/MainLayout";
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
-        <Header />
-        <MainLayout />
-      </div>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+      <Header />
+      <MainLayout />
+    </div>
   );
 }
 
--- src/layouts/MainLayout/Header.tsx
+++ src/layouts/MainLayout/Header.tsx
@@ -1,4 +1,4 @@
-import { Database } from "lucide-react";
+import { Database, Pickaxe } from "lucide-react";
 
 function Header() {
   return (
@@ -6,11 +6,12 @@ function Header() {
       <div className="flex items-center">
         <Database className="text-blue-400 mr-3" />
         <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
-          Blockchain Dashboard
+          BlockchainNet
         </h1>
       </div>
       <div className="flex items-center gap-4">
         <button className="bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 px-4 py-2 rounded-md flex items-center gap-2 transition-all duration-300 disabled:opacity-50">
+          <Pickaxe className="mr-2 text-white-600" size={20} />
           Mine New Block
         </button>
       </div>
